e1a163b0cf908c4976999711dcb1f023e553d1e2
Angel Gabriel Manzano Contreras <angel.manzano1326@alumnos.udg.mx>
2025-02-06 19:48:06 -0600

    Activity 1.7 improvements

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -21,9 +21,16 @@ const App = () => {
   useEffect(() => {
     // I isolated the calculation of the statistics
     // this way to use the most updated values of good, neutral and bad
+    const totalFeedback = good + neutral + bad
 
-    setAvarage( (good - bad) / (good + neutral + bad) )// Calculate the average
-    setPositive( (good / (good + neutral + bad)) * 100 )// Calculate the positive percentage
+    setAvarage( 
+      (good - bad) / ( totalFeedback ) 
+      || 0 // Assign 0 if the result is NaN
+    )// Doesn't consider the neutral feedback coz it's treated as 0
+    setPositive( 
+      (good / ( totalFeedback )) * 100 
+      || 0
+    )
   }, [good, neutral, bad])
   
   return (
@@ -53,7 +60,7 @@ const App = () => {
         <Result text="Bad" value={ bad } />
         <br />
         <Result text="Avarage" value={ avarage } />
-        <Result text="Positive" value={ positive } />
+        <Result text="Positive" value={ `${ positive } %` } />
       </section>
     </>
   )
